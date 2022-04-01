b52090eb07bda4bcb5f8c408ac0a7ea4ed7a0d72
mats9693 <982254249@qq.com>
2022-04-01 17:09:02 +0800

    binary search tree

--- demo/tree/binary_search_tree.go
+++ demo/tree/binary_search_tree.go
@@ -1,8 +1,56 @@
 package tree
 
 type binarySearchTree struct {
-
+	root *treeNode
 }
 
-// build tree
-// find value
+type treeNode struct {
+	value int
+
+	left  *treeNode
+	right *treeNode
+}
+
+func NewBST(data []int) *binarySearchTree {
+	return &binarySearchTree{
+		root: buildBST(data),
+	}
+}
+
+func(t *binarySearchTree) Find(v int) bool {
+    isExist := false
+
+    p := t.root
+    for p != nil {
+        if v == p.value {
+            isExist = true
+            break
+        } else if v < p.value {
+            p = p.left
+        } else { // v > p.value
+            p = p.right
+        }
+    }
+
+    return isExist
+}
+
+func buildBST(data []int) *treeNode {
+	if len(data) < 1 {
+		return nil
+	} else if len(data) == 1 {
+		return &treeNode{value: data[0]}
+	}
+
+	length := len(data)
+
+	middle := length/2 + length%2
+
+    root := &treeNode{value: data[middle]}
+
+    root.left = buildBST(data[:middle])
+
+    root.right = buildBST(data[middle+1:])
+
+    return root
+}
