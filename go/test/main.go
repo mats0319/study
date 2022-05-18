830d904c80f810011afdbf07acd9e3e2306104c9
mats9693 <982254249@qq.com>
2022-05-18 17:02:38 +0800

    daily

--- go/test/main.go
+++ go/test/main.go
@@ -1,29 +1,134 @@
 package main
 
-// 1. 变量地址的改变，并不意味着变量被重新分配，此处可能要结合“函数数据段堆栈”概念理解（即函数在内存中的样子，好像是这么叫吧）
-// 举例来说，我有两个结构体s1, s2，先让 s = s1，再让 s = s2；此时s变量显然没有被重新分配，而打印s变量的地址发生了变化（不能是空结构体）
+import (
+	"github.com/mats9693/utils/support"
+	"log"
+)
+
 func main() {
-	I.method(&s1{0})
+	data := support.GenerateRandomIntSlice(20, 100, 300)
 
-	I.method(&s2{0})
+	hashMapIns := newHashMap(data)
+
+	ok := hashMapIns.find(300) // true
+	if !ok {
+		log.Println("can't find exist item")
+		return
+	}
+	ok = hashMapIns.find(301) // false
+	if ok {
+		log.Println("find not exist item")
+		return
+	}
+
+	hashMapIns.modify(300, 400)
+	ok = hashMapIns.find(300) // false
+	if ok { // 'find' method has tested above
+		log.Printf("find not exist item del by 'modify'")
+		return
+	}
+	ok = hashMapIns.find(400) // true
+	if !ok { // 'find' method has tested above
+		log.Printf("can't find exist item insert by 'modify'")
+		return
+	}
+
+	hashMapIns.delete(300)
+	ok = hashMapIns.find(300) // false
+	if ok {
+		log.Println("delete failed")
+		return
+	}
+
+	log.Println("pass test")
 }
 
-type I interface {
-	method()
+type hashTable struct {
+	data []*listNode
 }
 
-type s1 struct {
-	i int
+type listNode struct {
+	exist bool
+
+	child []*listNode
 }
 
-func (s *s1) method() {
-	s.i = 1
+func newHashMap(data []int) *hashTable {
+	ins := &hashTable{
+		data: make([]*listNode, 10),
+	}
+
+	for i := range data {
+		ins.insert(uint(data[i]))
+	}
+
+	return ins
 }
 
-type s2 struct {
-	i int
+func (i *hashTable) insert(key uint) {
+	p := i.data
+	for key >= 10 {
+		index := key % 10
+		key /= 10
+
+		if p[index] == nil {
+			p[index] = &listNode{
+				child: make([]*listNode, 10),
+			}
+		}
+
+		p = p[index].child
+	}
+
+	if p[key] != nil { // 'key' exist
+		return
+	}
+
+	p[key] = &listNode{
+		exist: true,
+		child: make([]*listNode, 10),
+	}
 }
 
-func (s *s2) method() {
-	s.i = 2
+func (i *hashTable) find(key uint) bool {
+	node := i.getExistNode(key)
+
+	return node != nil && node.exist
+}
+
+func (i *hashTable) modify(oldKey uint, newKey uint) {
+	i.delete(oldKey)
+	i.insert(newKey)
+
+	return
+}
+
+func (i *hashTable) delete(key uint) {
+	node := i.getExistNode(key)
+
+	if node != nil {
+		node.exist = false
+	}
+}
+
+func (i *hashTable) getExistNode(key uint) *listNode {
+	p := i.data
+	isExist := true
+	for key >= 10 {
+		index := key % 10
+		key /= 10
+
+		if p[index] == nil { // not exist
+			isExist = false
+			break
+		}
+
+		p = p[index].child
+	}
+
+	if !isExist || p[key] == nil || !p[key].exist { // not exist
+		return nil
+	}
+
+	return p[key]
 }
